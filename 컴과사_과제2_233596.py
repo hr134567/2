433b74ba936d6ab728bf326b43251bf8e45cc524
master <haram040131@naver.com>
2023-05-08 15:43:42 +0900

    Add kiosk project

--- 컴과사_과제2_233596.py
+++ 컴과사_과제2_233596.py
@@ -1,16 +1,19 @@
 # 주차타워
 def tower_input() :
-    while True :
-        car_number = input("입고할 차량번호 뒷자리를 입력하세요 : ")
-        if len(car_number) == 4 :
-            print(f"{car_number} 차량이 입고되었습니다.")
-            break
+        if len(parking_tower) == 5 :
+            print("더 이상 차량을 입고할 수 없습니다.(최대 : 5대 / 현재 : 5대)")
         else :
-            print("4자리를 입력하세요.")
-    if len(parking_tower) == 5 :
-        print("더 이상 차량을 입고할 수 없습니다.(최대 : 5대 / 현재 : 5대)")
-    else :
-        parking_tower.append(car_number)
+            while True :
+                car_number = input("입고할 차량번호 뒷자리를 입력하세요 : ")
+                if len(car_number) == 4:
+                    if car_number in parking_tower:
+                        print("이미 입고되어 있습니다.")
+                    else:
+                        parking_tower.append(car_number)
+                        print(f"{car_number} 차량이 입고되었습니다.")
+                        break
+                else:
+                    print("4자리를 입력하세요.")
 def tower_output() :
     while True :
         car_number = input("출고할 차량번호 뒷자리를 입력하세요 : ")
