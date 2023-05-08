fcabe195e5f4a6179aad81c90ff99d644afffb21
master <haram040131@naver.com>
2023-05-08 14:10:55 +0900

    Add kiosk project

--- 컴과사_과제2_233596.py
+++ 컴과사_과제2_233596.py
@@ -1,5 +1,4 @@
 # 주차타워
-parking_tower = []
 def tower_input() :
     while True :
         car_number = input("입고할 차량번호 뒷자리를 입력하세요 : ")
@@ -24,7 +23,35 @@ def tower_output() :
         print(f"{car_number} 차량이 출고되었습니다.")
     else :
         print(f"{car_number} 차량이 존재하지 않습니다.")
+def tower_information() :
+    for i, car_number in enumerate(parking_tower) :
+        print(f"{i+1}층 : {car_number}")
+def tower_exit() :
+    print("프로그램을 종료합니다.")
+    exit()
+def tower_menu() :
+    print("●" * 30)
+    print("1. 차량입고")
+    print("2. 차량출고")
+    print("3. 차량입고정보")
+    print("4. 프로그램 종료")
+    print("●" * 30)
 
-tower_input()
-tower_output()
-print(parking_tower)
+parking_tower = []
+while True :
+    tower_menu()
+    while True :
+        menu_number = int(input("기능을 선택해주세요(1~4) : "))
+        if 1 <= menu_number <= 4 :
+            break
+        else :
+            print("1에서 4의 값만 입력해주세요.")
+    if menu_number == 1 :
+        tower_input()
+    elif menu_number == 2 :
+        tower_output()
+    elif menu_number == 3 :
+        tower_information()
+    else :
+        tower_exit()
+
